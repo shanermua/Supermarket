6513ccc7e26616f521d15c25c920f95a43691824
shanermua <shanermua@gmail.com>
2021-06-23 23:48:10 +0800

    shaner

--- Supermarket/Supermarket.cs
+++ Supermarket/Supermarket.cs
@@ -11,17 +11,22 @@ using System.Data.SqlClient;
 
 namespace Supermarket
 {
+    public class SQL
+    {
+        public static SqlConnection Connection { get; set; }
+    }
     public partial class Supermarket : Form
     {
         public Supermarket()
         {
             InitializeComponent();
-            SqlConnection connection = new SqlConnection();
+            SQL.Connection = new SqlConnection("Server=10.5.98.242;Database=Supermarket;User Id=sa;Password=<Shaner@wang.shaner.mua>;");
+            SQL.Connection.Open();
         }
 
         private void Supermarket_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("hello");
+            
         }
     }
 }

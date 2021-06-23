b282ba8efc91602ee014341a4877a478376febeb
shanermua <shanermua@gmail.com>
2021-06-23 20:55:39 +0800

    shaner

--- Supermarket/Form1.Designer.cs
+++ Supermarket/Form1.Designer.cs
@@ -35,11 +35,15 @@ namespace Supermarket
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.groupBox3);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
@@ -72,7 +76,7 @@ namespace Supermarket
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button1";
+            this.button2.Text = "货架管理";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -81,7 +85,7 @@ namespace Supermarket
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 29);
             this.button3.TabIndex = 1;
-            this.button3.Text = "button1";
+            this.button3.Text = "仓库管理";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -90,8 +94,35 @@ namespace Supermarket
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(94, 29);
             this.button4.TabIndex = 1;
-            this.button4.Text = "button1";
+            this.button4.Text = "供应商管理";
             this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(6, 157);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(355, 125);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "仓库";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(394, 63);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "刷新";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(494, 63);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 29);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "登录";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -101,6 +132,8 @@ namespace Supermarket
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -118,6 +151,9 @@ namespace Supermarket
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 

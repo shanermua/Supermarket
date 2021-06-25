e77f438b51de93bd830f58a87992a78dfb2455cf
Shaner <shanermua@gmail.com>
2021-06-25 14:00:54 +0800

    shaner

--- Supermarket/Supermarket.Designer.cs
+++ Supermarket/Supermarket.Designer.cs
@@ -29,17 +29,13 @@ namespace Supermarket
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.仓库状态 = new System.Windows.Forms.DataGridView();
+            this.货架状态 = new System.Windows.Forms.DataGridView();
+            this.销售列表 = new System.Windows.Forms.DataGridView();
             this.button7 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.仓库状态 = new System.Windows.Forms.DataGridView();
             this.button3 = new System.Windows.Forms.Button();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.销售列表 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.货架状态 = new System.Windows.Forms.DataGridView();
             this.button6 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
@@ -48,31 +44,57 @@ namespace Supermarket
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.groupBox1.SuspendLayout();
-            this.groupBox3.SuspendLayout();
+            this.button8 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.仓库状态)).BeginInit();
-            this.groupBox4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.销售列表)).BeginInit();
-            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.货架状态)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.销售列表)).BeginInit();
             this.groupBox5.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // 仓库状态
             // 
-            this.groupBox1.Controls.Add(this.groupBox3);
-            this.groupBox1.Controls.Add(this.groupBox4);
-            this.groupBox1.Controls.Add(this.groupBox2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(406, 674);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "状态";
+            this.仓库状态.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.仓库状态.Location = new System.Drawing.Point(0, 0);
+            this.仓库状态.Name = "仓库状态";
+            this.仓库状态.RowHeadersVisible = false;
+            this.仓库状态.RowHeadersWidth = 51;
+            this.仓库状态.RowTemplate.Height = 29;
+            this.仓库状态.Size = new System.Drawing.Size(748, 565);
+            this.仓库状态.TabIndex = 0;
+            // 
+            // 货架状态
+            // 
+            this.货架状态.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.货架状态.Location = new System.Drawing.Point(-4, 0);
+            this.货架状态.Name = "货架状态";
+            this.货架状态.RowHeadersVisible = false;
+            this.货架状态.RowHeadersWidth = 51;
+            this.货架状态.RowTemplate.Height = 29;
+            this.货架状态.Size = new System.Drawing.Size(752, 565);
+            this.货架状态.TabIndex = 0;
+            // 
+            // 销售列表
+            // 
+            this.销售列表.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.销售列表.Location = new System.Drawing.Point(-4, 0);
+            this.销售列表.Name = "销售列表";
+            this.销售列表.RowHeadersVisible = false;
+            this.销售列表.RowHeadersWidth = 51;
+            this.销售列表.RowTemplate.Height = 29;
+            this.销售列表.Size = new System.Drawing.Size(757, 569);
+            this.销售列表.TabIndex = 0;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(642, 504);
+            this.button7.Location = new System.Drawing.Point(964, 277);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(94, 29);
             this.button7.TabIndex = 1;
@@ -81,106 +103,43 @@ namespace Supermarket
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(684, 578);
+            this.button4.Location = new System.Drawing.Point(1006, 351);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(94, 29);
             this.button4.TabIndex = 1;
             this.button4.Text = "销售管理";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.仓库状态);
-            this.groupBox3.Location = new System.Drawing.Point(6, 236);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(390, 207);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "仓库";
-            // 
-            // 仓库状态
-            // 
-            this.仓库状态.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.仓库状态.Location = new System.Drawing.Point(6, 26);
-            this.仓库状态.Name = "仓库状态";
-            this.仓库状态.RowHeadersVisible = false;
-            this.仓库状态.RowHeadersWidth = 51;
-            this.仓库状态.RowTemplate.Height = 29;
-            this.仓库状态.Size = new System.Drawing.Size(378, 172);
-            this.仓库状态.TabIndex = 0;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(584, 578);
+            this.button3.Location = new System.Drawing.Point(906, 351);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 29);
             this.button3.TabIndex = 1;
             this.button3.Text = "仓库管理";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.销售列表);
-            this.groupBox4.Location = new System.Drawing.Point(6, 449);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(390, 204);
-            this.groupBox4.TabIndex = 0;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "销售";
-            // 
-            // 销售列表
-            // 
-            this.销售列表.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.销售列表.Location = new System.Drawing.Point(6, 26);
-            this.销售列表.Name = "销售列表";
-            this.销售列表.RowHeadersVisible = false;
-            this.销售列表.RowHeadersWidth = 51;
-            this.销售列表.RowTemplate.Height = 29;
-            this.销售列表.Size = new System.Drawing.Size(378, 172);
-            this.销售列表.TabIndex = 0;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(484, 578);
+            this.button2.Location = new System.Drawing.Point(806, 351);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 1;
             this.button2.Text = "货架管理";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.货架状态);
-            this.groupBox2.Location = new System.Drawing.Point(6, 26);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(390, 204);
-            this.groupBox2.TabIndex = 0;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "货架";
-            // 
-            // 货架状态
-            // 
-            this.货架状态.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.货架状态.Location = new System.Drawing.Point(6, 26);
-            this.货架状态.Name = "货架状态";
-            this.货架状态.RowHeadersVisible = false;
-            this.货架状态.RowHeadersWidth = 51;
-            this.货架状态.RowTemplate.Height = 29;
-            this.货架状态.Size = new System.Drawing.Size(378, 172);
-            this.货架状态.TabIndex = 0;
-            // 
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(124, 142);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(109, 29);
+            this.button6.Size = new System.Drawing.Size(96, 29);
             this.button6.TabIndex = 1;
             this.button6.Text = "登录";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(542, 504);
+            this.button1.Location = new System.Drawing.Point(864, 277);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 1;
@@ -189,7 +148,7 @@ namespace Supermarket
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(24, 710);
+            this.button5.Location = new System.Drawing.Point(770, 577);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(94, 29);
             this.button5.TabIndex = 1;
@@ -236,22 +195,76 @@ namespace Supermarket
             this.groupBox5.Controls.Add(this.label1);
             this.groupBox5.Controls.Add(this.textBox2);
             this.groupBox5.Controls.Add(this.textBox1);
+            this.groupBox5.Controls.Add(this.button8);
             this.groupBox5.Controls.Add(this.button6);
-            this.groupBox5.Location = new System.Drawing.Point(445, 12);
+            this.groupBox5.Location = new System.Drawing.Point(774, 41);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(374, 200);
             this.groupBox5.TabIndex = 4;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "账号";
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(237, 142);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(96, 29);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "注销";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(756, 598);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.销售列表);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(748, 565);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "销售列表";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.货架状态);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(748, 565);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "货架状态";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.仓库状态);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(748, 565);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "仓库状态";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
             // Supermarket
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(831, 789);
+            this.ClientSize = new System.Drawing.Size(1188, 620);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
@@ -261,40 +274,40 @@ namespace Supermarket
             this.Text = "超市管理系统";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Supermarket_FormClosing);
             this.Load += new System.EventHandler(this.Supermarket_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.仓库状态)).EndInit();
-            this.groupBox4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.销售列表)).EndInit();
-            this.groupBox2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.货架状态)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.销售列表)).EndInit();
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.DataGridView 仓库状态;
         private System.Windows.Forms.DataGridView 货架状态;
         private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.DataGridView 销售列表;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
